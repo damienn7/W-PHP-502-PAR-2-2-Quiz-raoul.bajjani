65617be88e78915c88282819ea4d45b64fd109d0
NokthoSolaire <lorenzo.zanuso@epitech.eu>
2023-05-24 09:49:25 +0200

    [UPDATED]Pull from Damien

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -9,15 +9,12 @@ use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
 use Doctrine\DBAL\Types\Types;
 use Symfony\Component\Security\Core\User\UserInterface;
 use Doctrine\ORM\Mapping as ORM;
-use Symfony\Component\Security\Core\User\PasswordAuthenticatedUserInterface;
+
 
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
 class User implements UserInterface, PasswordAuthenticatedUserInterface
 {
-
-
-
     #[ORM\Id]
     #[ORM\GeneratedValue]
     #[ORM\Column]
